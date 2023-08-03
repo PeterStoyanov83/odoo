92474fb2188b75684b7307787cb680033d328727
Petar Stoyanov <108938447+PeterStoyanov83@users.noreply.github.com>
2023-08-03 12:25:34 +0200

    Update __init__.py
    minor change. Makes it more readable.

--- odoo/addons/__init__.py
+++ odoo/addons/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-""" Addons module.
+"""
+Addons module.
 
 This module serves to contain all Odoo addons, across all configured addons
 paths. For the code to manage those addons, see odoo.modules.
@@ -12,8 +13,8 @@ known).
 
 This module also conveniently reexports some symbols from odoo.modules.
 Importing them from here is deprecated.
-
 """
+
 # make odoo.addons a namespace package, while keeping this __init__.py
 # present, for python 2 compatibility
 # https://packaging.python.org/guides/packaging-namespace-packages/
